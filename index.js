986db14d149e60f0528cc1851f205d18fe8742a7
hulmgulm <hulmgulm@users.noreply.github.com>
2025-12-09 08:52:57 +0100

    Refactor server to use env variables and enable HTTPS
    Refactor server configuration to use environment variables for cache store path, port, token, and max age. Update server to use HTTPS and add logging for cache operations.

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 import express from 'express';
-import http from 'node:http';
+import https from 'node:https';
 import path from 'node:path';
-import { createWriteStream } from 'node:fs';
+import { createWriteStream, readFileSync } from 'node:fs';
+import { spawn } from 'node:child_process';
 
-const store = `${import.meta.dirname}/store`;
-const port = 4711;
-const token = 'sdfsdf';
+// configuration
+const store = process.env.CACHE_STORE_PATH || `${import.meta.dirname}/store`;
+const port = process.env.CACHE_PORT || 443;
+const token = process.env.CACHE_TOKEN || 'do_not_use_this_token';
+const maxAge = process.env.CACHE_MAX_AGE || 21;
 
 const app = express();
 
 // Check Authorization header
 app.use((req, res, next) => {
   if (req.headers.authorization !== `Bearer ${token}`) {
-    return res.status(401).send('Missing or invalid authentication token.');
+    console.log('nx-cache', 'Error:', new Date(), 'Missing or invalid authentication token');
+    return res.status(401).send('Missing or invalid authentication token');
   }
   next();
 });
@@ -25,19 +29,55 @@ app.put('/v1/cache/:hash', async (req, res) => {
   req.pipe(writeStream);
 
   writeStream.on('finish', () => {
+    console.log('nx-cache', new Date(), 'Added to cache', req.params.hash);
     res.status(202).send(`Successfully uploaded ${req.params.hash}`);
   });
 
   writeStream.on('error', (err) => {
-    res.status(409).send('File already exists.');
+    if (err.code !== 'EEXIST') {
+      console.log('nx-cache', new Date(), 'Error writing to cache:', err);
+      res.status(501).send('Error writing to cache');
+    } else {
+      console.log('nx-cache', new Date(), 'Already in cache:', req.params.hash);
+      res.status(409).send('File already exists.');
+    }
   });
 });
 
+// touch file in cache to allow deleting unused files
+const setHeaders = (res, path) => {
+  console.log('nx-cache', new Date(), 'Cache access:', path.split('/').pop());
+  spawn('touch', [path], { stdio: 'ignore' });
+}
+
 // handle cache get
-app.use('/v1/cache/', express.static(store), (req, res) => {
+app.use('/v1/cache/', express.static(store, { setHeaders }), (req, res) => {
+  console.log('nx-cache', new Date(), 'Not in cache:', req.url.replace('/', ''));
   res.status(404).send('Error: Not Found');
 });
 
 // start server
-const server = http.createServer(app);
-server.listen(port, () => { console.log(`Server is listening at http://localhost:${port}`); });
+var key = readFileSync('selfsigned.key');
+var cert = readFileSync('selfsigned.crt');
+const server = https.createServer({ key, cert }, app);
+server.listen(port, () => { console.log('nx-cache', new Date(), `Server is listening at https://0.0.0.0:${port}`); });
+
+// cache clean up
+const cleanup = () => {
+  console.log('nx-cache', new Date(), 'Running cache clean up', [store, '-type', 'f', '-mtime', `+${maxAge}`, '-print', '-delete']);
+  const find = spawn('find', [store, '-type', 'f', '-mtime', `+${maxAge}`, '-print', '-delete']);
+  find.stdout.on('data', (data) => {
+    console.log('nx-cache', new Date(), 'Result of cache clean up:', data.toString());
+  });
+
+  find.stderr.on('data', (data) => {
+    console.log('nx-cache', new Date(), 'Error cleaning cache:', data.toString());
+  });
+
+  find.on('close', (code) => {
+    console.log('nx-cache', new Date(), 'Cache clean up finished with code:', code);
+  });
+};
+
+setInterval(cleanup, 1000 * 60 * 60 * 5); // run every five hours
+cleanup(); // run once at startup
